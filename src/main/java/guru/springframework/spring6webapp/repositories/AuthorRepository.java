8f00096b84a5b6d0b706afd444c826f23162c600
Amurita <amurita.dev@gmail.com>
2024-04-03 09:36:04 +0200

    Added Spring Data repository package and an initial AuthorRepository Interface.

---
+++ src/main/java/guru/springframework/spring6webapp/repositories/AuthorRepository.java
@@ -0,0 +1,9 @@
+package guru.springframework.spring6webapp.repositories;
+
+/**
+ * Created by Amurita
+ * On Mittwoch, the 03. of April, 2024
+ * At 09:34 o'clock
+ */
+public interface AuthorRepository {
+}
