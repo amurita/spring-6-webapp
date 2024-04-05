c6079cb13b3afaac22617c5d408329c77ede5566
Amurita <amurita.dev@gmail.com>
2024-04-05 11:55:11 +0200

    6-publisher-relationship-own-version:   - adjusted BootstrapData to have both entity relationships created properly (Books and Authors bidirectional associated),   - enabled H2 console via application.properties.

--- src/main/java/guru/springframework/spring6webapp/bootstrap/BootstrapData.java
+++ src/main/java/guru/springframework/spring6webapp/bootstrap/BootstrapData.java
@@ -18,24 +18,17 @@ import org.springframework.stereotype.Component;
 @Component
 public class BootstrapData implements CommandLineRunner {
 
-    /*
-    * Components (final variables) from the Spring context
-    * */
     private final AuthorRepository authorRepository;
     private final BookRepository bookRepository;
-
     private final PublisherRepository publisherRepository;
 
-    public BootstrapData(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
+    public BootstrapData(AuthorRepository authorRepository, BookRepository bookRepository,
+                         PublisherRepository publisherRepository) {
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
         this.publisherRepository = publisherRepository;
-  ;  }
+    }
 
-    /**
-     * @param args
-     * @throws Exception
-     */
     @Override
     public void run(String... args) throws Exception {
         Author eric = new Author();
@@ -45,6 +38,90 @@ public class BootstrapData implements CommandLineRunner {
         Book ddd = new Book();
         ddd.setTitle("Domain Driven Design");
         ddd.setIsbn("123456");
+
+        Author ericSaved = authorRepository.save(eric);
+        Book dddSaved = bookRepository.save(ddd);
+
+        Author rod = new Author();
+        rod.setFirstName("Rod");
+        rod.setLastName("Johnson");
+
+        Book noEJB = new Book();
+        noEJB.setTitle("J2EE Development without EJB");
+        noEJB.setIsbn("54757585");
+
+        Author rodSaved = authorRepository.save(rod);
+        Book noEJBSaved = bookRepository.save(noEJB);
+
+        ericSaved.getBooks().add(dddSaved);
+        rodSaved.getBooks().add(noEJBSaved);
+        dddSaved.getAuthors().add(ericSaved);
+        noEJBSaved.getAuthors().add(rodSaved);
+
+
+        Publisher publisher = new Publisher();
+        publisher.setPublisherName("My Publisher");
+        publisher.setAddress("123 Main");
+        Publisher savedPublisher = publisherRepository.save(publisher);
+
+        dddSaved.setPublisher(savedPublisher);
+        noEJBSaved.setPublisher(savedPublisher);
+
+        authorRepository.save(ericSaved);
+        authorRepository.save(rodSaved);
+        bookRepository.save(dddSaved);
+        bookRepository.save(noEJBSaved);
+
+        System.out.println("In Bootstrap");
+        System.out.println("Author Count: " + authorRepository.count());
+        System.out.println("Book Count: " + bookRepository.count());
+
+
+
+        System.out.println("Publisher Count: " + publisherRepository.count());
+    }
+}
+
+/*@Component
+public class BootstrapData implements CommandLineRunner {
+
+    *//*
+    * Components (final variables) from the Spring context
+    * *//*
+    private final AuthorRepository authorRepository;
+    private final BookRepository bookRepository;
+
+    private final PublisherRepository publisherRepository;
+
+    public BootstrapData(AuthorRepository authorRepository,
+                         BookRepository bookRepository,
+                         PublisherRepository publisherRepository) {
+        this.authorRepository = authorRepository;
+        this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
+    }
+
+    *//**
+     * @param args
+     * @throws Exception
+     *//*
+    @Override
+    public void run(String... args) throws Exception {
+        Author eric = new Author();
+        eric.setFirstName("Eric");
+        eric.setLastName("Evans");
+
+        Book ddd = new Book();
+        ddd.setTitle("Domain Driven Design");
+        ddd.setIsbn("123456");
+
+        Author rod = new Author();
+        rod.setFirstName("Rod");
+        rod.setLastName("Johnson");
+
+        Book noEJB = new Book();
+        noEJB.setTitle("J2EE- Development without EJB");
+        noEJB.setIsbn("54757585");
 
         Publisher xyzPublisher = new Publisher();
         xyzPublisher.setPublisherName("XYZ-Publisher");
@@ -59,22 +136,17 @@ public class BootstrapData implements CommandLineRunner {
         Author ericSaved = authorRepository.save(eric);
         Book dddSaved = bookRepository.save(ddd);
 
-        Publisher xyzPublisherSaved = publisherRepository.save(xyzPublisher);
-        Publisher abcPublisherSaved = publisherRepository.save(abcPublisher);
-
-        Author rod = new Author();
-        rod.setFirstName("Rod");
-        rod.setLastName("Johnson");
-
-        Book noEJB = new Book();
-        noEJB.setTitle("J2EE- Development without EJB");
-        noEJB.setIsbn("54757585");
-
         Author rodSaved = authorRepository.save(rod);
         Book noEJBSaved = bookRepository.save(noEJB);
 
+        Publisher xyzPublisherSaved = publisherRepository.save(xyzPublisher);
+        Publisher abcPublisherSaved = publisherRepository.save(abcPublisher);
+
         ericSaved.getBooks().add(dddSaved);
         rodSaved.getBooks().add(noEJBSaved);
+
+        dddSaved.getAuthors().add(ericSaved);
+        noEJBSaved.getAuthors().add(rodSaved);
 
         authorRepository.save(ericSaved);
         authorRepository.save(rodSaved);
@@ -101,7 +173,9 @@ public class BootstrapData implements CommandLineRunner {
         System.out.println("Publisher :\n" + xyzPublisherSaved.toString());
         System.out.println("Publisher :\n" + abcPublisherSaved.toString());
 
-        System.out.println("\nBook " + dddSaved.getTitle() + " has publisher: " + dddSaved.getPublisher() + ".\n");
-        System.out.println("Book " + noEJBSaved.getTitle() + " has publisher: " + noEJBSaved.getPublisher() + ".\n");
+        System.out.println("\nBook " + dddSaved.getTitle() + " has publisher: "
+                + dddSaved.getPublisher() + ".\n");
+        System.out.println("Book " + noEJBSaved.getTitle() + " has publisher: "
+                + noEJBSaved.getPublisher() + ".\n");
     }
-}
+}*/
